6855283653e718076bc25071a20c0ecb88d78ce7
Javier Costanzi <javier.costanzi@gmail.com>
2017-08-06 17:23:50 -0300

    Subo archivos del proyecto del laboratorio 2 terminado.

---
+++ Cristian.Laboratorio2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cristian.Laboratorio2.Startup))]
+namespace Cristian.Laboratorio2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
